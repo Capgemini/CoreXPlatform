2ae13a2be985d97a45115587291d20411850fc6a
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2017-11-21 13:00:56 +0000

    start testing

--- src/CoreXPlatform.API/Controllers/ValuesController.cs
+++ src/CoreXPlatform.API/Controllers/ValuesController.cs
@@ -9,6 +9,11 @@ namespace CoreXPlatform.API.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
--- test/CoreXPlatform.API.Tests/ValuesControllerTests.cs
+++ test/CoreXPlatform.API.Tests/ValuesControllerTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace CoreXPlatform.API.Tests
 {
-    public class UnitTest1
+    public class ValuesControllerTests
     {
         [Fact]
         public void Test1()
