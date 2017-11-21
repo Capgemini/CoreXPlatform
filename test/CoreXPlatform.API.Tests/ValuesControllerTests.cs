94720fd2011ad403510a1d4beeb41de013fe6e79
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2017-11-21 14:07:59 +0000

    add a sample test

--- test/CoreXPlatform.API.Tests/ValuesControllerTests.cs
+++ test/CoreXPlatform.API.Tests/ValuesControllerTests.cs
@@ -1,14 +1,24 @@
-using System;
-using Xunit;
-
 namespace CoreXPlatform.API.Tests
 {
+    using System;
+    using System.Linq;
+    using CoreXPlatform.API.Controllers;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using Xunit;
+
     public class ValuesControllerTests
     {
         [Fact]
-        public void Test1()
+        public void ValuesController_Get_Ok()
         {
+            var controller = new ValuesController(new Mock<ILogger<ValuesController>>().Object);
 
+            var result = controller.Get();
+
+            Assert.NotNull(result);
+
+            Assert.Equal(2, result.Count());
         }
     }
 }
